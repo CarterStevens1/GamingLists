d750f53b1378a0fc196648e2e6b665c533cc16eb
CarterStevens1 <CarterStevens1@users.noreply.github.com>
2020-12-16 20:37:18 +0000

    css changes

--- wwwroot/lib/bootstrap/dist/js/bootstrap.js
+++ wwwroot/lib/bootstrap/dist/js/bootstrap.js
@@ -1551,7 +1551,9 @@
     DATA_TOGGLE: '[data-toggle="dropdown"]',
     FORM_CHILD: '.dropdown form',
     MENU: '.dropdown-menu',
-    NAVBAR_NAV: '.navbar-nav',
+
+
+      _NAV: '.navbar-nav',
     VISIBLE_ITEMS: '.dropdown-menu .dropdown-item:not(.disabled):not(:disabled)'
   };
   var AttachmentMap = {
